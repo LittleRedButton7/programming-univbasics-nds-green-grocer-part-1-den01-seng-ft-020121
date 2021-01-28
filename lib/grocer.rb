10a030a508d98b62cd4e0934168619770b084cb8
Marissa Nolan <ritz1484@cs.com>
2021-01-28 19:10:30 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -30,6 +30,7 @@ def consolidate_cart(cart)
     new_item = find_item_by_name_in_collection(cart[count][:item], new_cart)
     if new_item != nil
       new_item[:count] += 1
+    else 
     end
    count += 1
   end
