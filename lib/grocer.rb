e501ab234918a9795cf3d304118fb5010b3927ad
Marissa Nolan <ritz1484@cs.com>
2021-01-28 19:06:49 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -27,7 +27,7 @@ def consolidate_cart(cart)
   new_cart = []
   count = 0
   while count < cart.length 
-    new_item = find_item_by_name_in_collection
+    new_item = find_item_by_name_in_collection(cart[count][:item])
   
    count += 1
   end
